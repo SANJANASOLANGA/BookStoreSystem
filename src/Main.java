cf94fded35074b4f50a66b44b22b27c64ac27436
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:51:20 +0530

    Created ebook2 object

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         // Create some books
         Book ebook = new Ebook(1, "ගම්පෙරළිය", "Martin Wickramasinghe", 1229.99, "PDF");
+        Book ebook2 = new Ebook(1, "ගම්පෙරළිය", "Martin Wickramasinghe", 1229.99, "PDF");
         Book physicalBook = new PhysicalBook(2, "මඩොල් දූව", "Martin Wickramasinghe", 1539.99, 0.85, 5.00);
 
         // Create a customer
-        Customer customer = new Customer(1, "Alice Smith", "alice.smith@example.com");
+        Customer customer = new Customer(1, "Amal Perera", "amalperera@gmail.com");
 
         // Create a shopping cart and add books
         ShoppingCart cart = new ShoppingCart();
