32901cc85bb8cd54e209c71c040f8cc6ac39087f
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:25:02 +0530

    Added variables for a Customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,13 @@
 public class Customer {
+    private int id;
+    private String name;
+    private String email;
+
+    public Customer(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+
 }
