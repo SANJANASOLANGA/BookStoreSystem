b58db67539d2722d7d6fec9a2a19e514babd23f4
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:28:28 +0530

    Added constructor

--- src/PhysicalBook.java
+++ src/PhysicalBook.java
@@ -1,9 +1,11 @@
 public class PhysicalBook extends Book {
     private double weight;
+    private double shippingCost;
 
     public PhysicalBook(int id, String title, String author, double price, double weight, double shippingCost) {
         super(id, title, author, price);
         this.weight = weight;
+        this.shippingCost = shippingCost;
     }
 
     @Override
