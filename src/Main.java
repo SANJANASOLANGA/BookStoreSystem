5a1ad7e230350e926cb0cce3ddcb5349f59cd464
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:49:23 +0530

    Created customer object

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         Book ebook = new Ebook(1, "ගම්පෙරළිය", "Martin Wickramasinghe", 1229.99, "PDF");
         Book physicalBook = new PhysicalBook(2, "මඩොල් දූව", "Martin Wickramasinghe", 1539.99, 0.85, 5.00);
 
+        // Create a customer
+        Customer customer = new Customer(1, "Alice Smith", "alice.smith@example.com");
+
 
     }
 }
