e843eeb2d1c7b277f55af6614ddc24f094c21135
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:21:28 +0530

    Added format for the class

--- src/Ebook.java
+++ src/Ebook.java
@@ -1,2 +1,14 @@
-public class Ebook {
+public class Ebook extends Book {
+    private String format;
+
+    public Ebook(int id, String title, String author, double price, String format) {
+        super(id, title, author, price);
+        this.format = format;
+    }
+
+    @Override
+    public String getDetails() {
+        return getTitle() + " by " + getAuthor() + " (Format: " + format + ")";
+    }
 }
+
