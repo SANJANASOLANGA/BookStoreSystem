1b9dec30ac0fadee5c2b0c57de41edf859e3a69d
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:29:07 +0530

    Added methods for adding and removing book

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private List<Book> cartItems;
+
+    public ShoppingCart() {
+        cartItems = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        cartItems.add(book);
+    }
+
+    public void removeBook(Book book) {
+        cartItems.remove(book);
+    }
+
+
 }
+
