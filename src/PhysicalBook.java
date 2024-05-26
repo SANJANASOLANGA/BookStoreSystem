7c80f4e4f941cc976996e927573316f6cacb56a1
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:23:22 +0530

    Added weight for the class

--- src/PhysicalBook.java
+++ src/PhysicalBook.java
@@ -1,2 +1,14 @@
-public class PhysicalBook {
+public class PhysicalBook extends Book {
+    private double weight;
+
+    public PhysicalBook(int id, String title, String author, double price, double weight, double shippingCost) {
+        super(id, title, author, price);
+        this.weight = weight;
+    }
+
+    @Override
+    public String getDetails() {
+        return getTitle() + " by " + getAuthor() + " (Weight: " + weight + "kg)";
+    }
 }
+
