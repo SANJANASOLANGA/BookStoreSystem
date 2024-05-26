2fb7e942f29c5caa353ad1d057b771f0ab032d36
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:18:18 +0530

    Added getters for a Book

--- src/Book.java
+++ src/Book.java
@@ -4,5 +4,17 @@ public abstract class Book {
     private String author;
     private double price;
 
+    public Book(int id, String title, String author, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public double getPrice() { return price; }
+
 
 }
