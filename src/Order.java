c801126ba48ecd392a876e53470bdb7b8f3a71c1
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-26 23:32:36 +0530

    Added variables for Order

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class Order {
+    private int orderId;
+    private Customer customer;
+    private List<Book> books;
+    private double totalAmount;
+
+    public Order(int orderId, Customer customer, List<Book> books) {
+        this.orderId = orderId;
+        this.customer = customer;
+        this.books = books;
+    }
+
 }
